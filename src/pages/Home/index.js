01ca76f2d5455420138e4032de7bf9a36820f450
dylanlaborde <dylan4047@gmail.com>
2024-10-07 20:08:12 +0200

    correction of invalid data before render on EventCard , we now verified if the value exist we render the card else nothing

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -117,13 +117,16 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
-        <EventCard
+        {last ? (
+          <EventCard
           imageSrc={last?.cover}
           title={last?.title}
           date={new Date(last?.date)}
           small
           label="boom"
         />
+        ):null
+        }
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
